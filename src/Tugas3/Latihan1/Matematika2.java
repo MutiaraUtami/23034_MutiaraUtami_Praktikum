094ae4552350a5617493d0043c8a4d33a0733475
USER <USER@192.168.0.104>
2024-09-29 15:22:57 +0700

    first commmit

---
+++ src/Tugas3/Latihan1/Matematika2.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Tugas3.Latihan1;
+
+import Tugas2.Latihan1.Matematika;
+
+public class Matematika2 extends Matematika{  
+    public void modulus(int a, int b) {
+        System.out.println("Modulus: " + a + " % " + b + " = " + (a%b));
+    }
+}
